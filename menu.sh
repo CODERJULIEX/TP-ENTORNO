1350eb00a6e25d9d034d2d9f6791da902ad69ae2
ignacio <ignacio.cicoria@gmail.com>
2023-07-06 13:11:05 -0300

    modificacion menu. Faltan archivos a ejecutar

--- menu.sh
+++ menu.sh
@@ -1,23 +1,26 @@
-#!/bin/bash 
+#!/bin/bash
 
-sal=0 
+sal=0
 
-while [ $sal -ne 6 ] ; do 
-echo "1: Generar imagenes" 
-echo "2: Descargar" 
-echo "3: Descomprimir" 
-echo "4: Procesar" 
-echo "5: Comprimir" 
-echo "6: Salir" 
+while true ; do
+echo "1: Generar imagenes"
+echo "2: Descargar"
+echo "3: Descomprimir"
+echo "4: Procesar"
+echo "5: Comprimir"
+echo "6: Salir"
 
-read -p "Seleccione una opcion" sal 
+	read -p "Seleccione una opcion:  " sal
 
-case $sal in 
-       1) echo "1"  
-       ;; 
-       2) echo "2" 
-       ;; 
-       3) echo "3" 
-       ;; 
-       4) echo "4" 
-       ;; 
+	case $sal in
+		1) echo " Has seleccionado generar imagenes ";;
+		2) echo "Has seleccionado descargar";;
+		3) echo "Has seleccionado descomprimir ";;
+		4) echo "Has seleccionado procesar";;
+		5) echo "Has seleccionado comprimir";;
+		6) echo "Has seleccionado salir. Haste luego"
+		break;;
+		*) echo $sal "no es una opcion"
+
+	esac
+done
