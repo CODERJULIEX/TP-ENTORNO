47bea0260534e6cc2a59605d2c2024a6d6a13f79
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-07 12:06:34 -0300

    primeras parte del script, solo agregue el for que descarga la catidad de imagenes ingresadas como argumento, no pude todavia leer bien el archivo de nombres

--- generar.sh
+++ generar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#descarga la cantidad de imagenes ingresada como argumento con el nombre imagen.jpg, tiene un sleep de 2seg entre cada descarga
+NAME='imagen.jpg'
+for (( i = 0; i <$1; i++ ))
+do
+	wget --default-page=$NAME https://thispersondoesnotexist.com
+	sleep 2s
+done
 
-echo 'generar'
-echo 'test de modificar un archivo'
-echo 'muestra de modificacion 3'
+exit 0
