00cc8e4d173237ea3d0bcc504b461c7ea4fee565
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-08 10:37:59 -0300

    agregue los comandos para generar un numero aleatorio y luego leer el nombre de esa lina del archivo csv, agregue que cree la carpe imagenes y generes las imagenes dentro de esa carpeta.

--- generar.sh
+++ generar.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #descarga la cantidad de imagenes ingresada como argumento con el nombre imagen.jpg, tiene un sleep de 2seg entre cada descarga
-NAME='imagen.jpg'
+
+mkdir -p "imagenes"
+cd "imagenes"
+ARCHIVO="dict.csv"
 for (( i = 0; i <$1; i++ ))
 do
-	wget --default-page=$NAME https://thispersondoesnotexist.com
+	LINEA=$((`echo $(($RANDOM%30686))` + `echo $(($RANDOM))` + `echo $(($RANDOM))`))
+	DATOS=$(sed -n "${LINEA}p" "$ARCHIVO")
+	NOMBRE=$(echo "$DATOS" | awk -F ',' '{print $1}')
+	echo $NOMBRE
+	wget --default-page="$NOMBRE" https://thispersondoesnotexist.com
 	sleep 2s
 done
 
