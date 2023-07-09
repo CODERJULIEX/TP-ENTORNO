74f76141e989f73cb04b4a337691592f2e281648
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-09 15:54:31 -0300

    agregue la suma de verificaciones y la validacion de que ingrese un argumento y que el numero sea mayor a 0. Ya esta terminado

--- generar.sh
+++ generar.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #descarga la cantidad de imagenes ingresada como argumento con el nombre imagen.jpg, tiene un sleep de 2seg entre cada descarga
 
+if [ $# -eq 0 ]
+then
+	echo "ERROR no se ingreso ningun argumento"
+	exit 1
+else
+	if [ $1 -le 0 ]
+	then
+		echo "ERROR ingrese una cantidad de imagenes valida"
+		exit 2
+	fi
+fi
 mkdir -p "imagenes"
 cd "imagenes"
 rm -f "dict.csv"
@@ -11,15 +22,14 @@ do
 	LINEA=$((`echo $(($RANDOM%30686))` + `echo $(($RANDOM))` + `echo $(($RANDOM))`))
 	DATOS=$(sed -n "${LINEA}p" "$ARCHIVO")
 	NOMBRE=$(echo "$DATOS" | awk -F ',' '{print $1}')
-	echo $NOMBRE
 	wget --default-page="$NOMBRE" https://thispersondoesnotexist.com
 	sleep 2s
 done
 
 rm -f "dict.csv"
 cd ..
-touch suma_de_verificaciones.txt
-md5sum imagenes >> suma_de_verificaciones.txt
+rm -f "suma_de_verificaciones.txt"
+find ./imagenes -type f -exec md5sum {} + | awk '{print $1}' | sort | md5sum > suma_de_verificacion.txt
 tar -czvf imagenes.tar imagenes 
 
 exit 0
