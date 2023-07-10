583486e07b1ede90c4e20849998b05b537c91b45
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-09 23:31:31 -0300

    cambie varias cosas del programa, agregue la comprobacion de la suma de verificacion, estaria terminado por ahora pero seguramente cuando unamos todas las ramas haya algo que ajustar

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #Se debe poder indicar por argumento dos archivos (uno
 #con las imágenes comprimidas y otro con una suma de verificación). Si
 #ocurrió algún error se debe informar al usuario de lo contrario se procede
-#a descomprimir.
+#a descomprimir
 
+if [ $# -ne 2 ]; then
+  	echo "Error: Se requieren dos argumentos. Uso: $0 <imagenes.tar> <suma_de_verificaciones.txt>"
+  	exit 2
+fi
+
+if [ ! -f "$2" ]; then
+  	echo "Error: El archivo de suma de verificación '$suma_de_verificaciones.txt' no existe."
+  	exit 3
+fi
 
 ARCHIVO="$1"
-
+SUMA_DE_VERIFICACION="./suma_de_verificacion.txt"
 if [ -f "$ARCHIVO" ]
 then
-        echo "El archivo $archivo existe."
+        echo "El archivo $ARCHIVO existe."
         tar -xvf "$ARCHIVO" 
-        echo "El archivo $archivo fue descomprimido."
+        echo "El archivo $ARCHIVO fue descomprimido."
+        find ./imagenes -type f -exec md5sum {} + | awk '{print $1}' | sort | md5sum > verificacion.txt
+        VERIFICACION="./verificacion.txt"
+        if cmp -s "$VERIFICACION" "$SUMA_DE_VERIFICACION" 
+        then
+                echo "suma de verificacion correcta"
+        else
+		echo "Error: Las sumas de verificaciones no coinciden"
+		exit 4
+	fi
 else
-        echo "El archivo $archivo no existe."
+        echo "El archivo $ARCHIVO no existe."
+        exit 1
 fi 
-=======
-#Verificar que se proporcionen dos argumentos
-if [ $# -ne 2 ]; then
-  echo "Error: Se requieren dos argumentos. Uso: $0 <imagenes.tar> <suma_de_verificaciones.txt>"
-  exit 1
-fi
 
-#Obtener nombres de archivos de los argumentos
-imagenes.tar=$1
-suma_de_verificaciones.txt=$2
-
-#Verificar la existencia de los archivos
-if [ ! -f "$imagenes.tar" ]; then
-  echo "Error: El archivo de imágenes comprimidas '$imagenes.tar' no existe."
-  exit 1
-fi
-
-if [ ! -f "$suma_de_verificaciones.txt" ]; then
-  echo "Error: El archivo de suma de verificación '$suma_de_verificaciones.txt' no existe."
-  exit 1
-fi
-
-#Descomprimir las imágenes
-tar -xf "$imagenes.tar" -C .
-
-echo "Descompresión completada."
- 
->>>>>>> 211a9b49ce3092663e129d18ade2bd03573cbcd7
+exit 0
 
