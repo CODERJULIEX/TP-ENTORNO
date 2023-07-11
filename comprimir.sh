5f0a3938e3ca470bb7ed18d1b22cf45e764a7cb0
ignacio <ignacio.cicoria@gmail.com>
2023-07-11 13:38:08 -0300

    modificacion de comprimir

--- comprimir.sh
+++ comprimir.sh
@@ -19,24 +19,22 @@ rm -f ../lista_nombres_validos.txt
 touch ../lista_nombres_validos.txt
 rm -f ../lista_nombres_terminan_en_a.txt
 touch ../lista_nombres_terminan_en_a.txt
-LISTA_NOMBRES=../lista_nombres.txt
 
-while IFS= read -r nombre
+#Al establecer IFS vacio hace que trate toda la linea como una cadena sin dividirla y asignandole la variable nombre
+while IFS= read -r nombre < "../lista_nombres.txt"
 do
-
 	ultimo_caracter="${nombre: -1}"
 	if [[ $nombre =~ ^[A-Z][a-z]+ ]]
 	then
 		echo "$nombre" >> ../lista_nombres_validos.txt
-
 	fi
 
 	if [[ "$ultimo_caracter" == "a" ]]
 	then
 		echo "$nombre" >> ../lista_nombres_terminan_en_a.txt
-
 	fi
-done < "$LISTA_NOMBRES"
+done
+
 
 cd ..
 mkdir -p archivos
@@ -49,4 +47,5 @@ tar -cvf archivos.tar archivos
 #crea un volumen montado y vincula el host con el contenedor, luego copia el archivo de una carpeta a otra
 docker run -it -v "$(pwd)/archivos_datos:/archivos" ubuntu cp ./archivos/archivos.tar /archivos_datos/
 
+echo " Archivo comprimido copiado en la carpeta archivos_datos del host "
 exit 0
