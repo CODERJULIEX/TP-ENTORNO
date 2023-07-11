6bf15995b0f78e217e2678dbc8f71a63498bbb83
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-11 14:42:55 -0300

    modificacion de comentarios

--- comprimir.sh
+++ comprimir.sh
@@ -17,7 +17,8 @@ done
 touch ../lista_nombres_validos.txt					#crea la nueva lista_nombres_validos
 touch ../lista_nombres_terminan_en_a.txt				#crea la nueva lista_nombres_terminan_en_a
 while IFS= read -r NOMBRE < "../lista_nombres.txt"                      #permanece en el bucle mientras NOMBRE toma como valor los nombres de la lista_nombres.txt.
-                                                                        #al establecer IFS como vacio trata toda la linea como una cadena, sin cortarla, y la asigna a la variable NOMBRE
+                                                                        #al establecer IFS como vacio trata toda la linea como una cadena, sin cortarla, y la asigna a la
+									#variable NOMBRE
 do
 
         ULTIMO_CARACTER="${NOMBRE: -1}"                                 #se guarda en una variable el ultimo caractes del nombre
@@ -42,7 +43,8 @@ mv ./lista_nombres_terminan_en_a.txt ./archivos                         #se muev
 cp -r ./imagenes ./archivos                                             #se copia la carpeta imagenes dentro de archivos
 tar -cvf archivos.tar archivos                                          #se comprime la carpeta archivos
 
-docker run -it -v "$(pwd)/archivos_datos:/archivos" ubuntu cp ./archivos/archivos.tar /archivos_datoS/ #crea un volumen montado y vincula el host con el contenedor, luego copia el archivo de una carpeta a otra
+docker run -it -v "$(pwd)/archivos_datos:/archivos" ubuntu cp ./archivos/archivos.tar /archivos_datoS/ 	#crea un volumen montado y vincula el host con el contenedor, luego
+													#copia el archivo de una carpeta a otra
 
 echo " El archivo comprimido 'archivo.tar' se guardara en la carpeta archivos_datos" #imprime por pantalla donde encontrar el archivo creado
 exit 0
