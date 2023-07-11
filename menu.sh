36e189abe92f2f46962985114b35ba749451aaa4
DuclosNicolas <duclosnicolas.00@gmail.com>
2023-07-11 10:35:46 -0300

    Estoy terminando de agregar las partes del menu para que funciones los scrips que nesesitan paramentros. Todavia no termine pero me tengo que mover a otra branch

--- menu.sh
+++ menu.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-sal=0
-
-while true ; do
-echo "1: Generar imagenes"
-echo "2: Descargar"
-echo "3: Descomprimir"
-echo "4: Procesar"
-echo "5: Comprimir"
-echo "6: Salir"
-
-	read -p "Seleccione una opcion: " sal
-
+while true
+do
+	echo "1: Generar imagenes"
+	echo "2: Descargar"
+	echo "3: Descomprimir"
+	echo "4: Procesar"
+	echo "5: Comprimir"
+	echo "6: Salir"
+	echo "Seleccione una opcion: "
+	read sal
 	case $sal in
-		1) echo " Has seleccionado generar imagenes "
-		bash generar.sh;;
-		2) echo "Has seleccionado descargar"
-		bash descargar.sh;;
-		3) echo "Has seleccionado descomprimir "
-		bash descomprimir.sh;;
-		4) echo "Has seleccionado procesar"
-		bash procesar.sh;;
-		5) echo "Has seleccionado comprimir"
-		bash comprimir.sh;;
-		6) echo "Has seleccionado salir. Haste luego"
-		break;;
-		*) echo "$sal no es una opcion valida"
+		1)
+			echo "Has seleccionado generar imagenes "
+			echo "Ingrese la cantidad de imagenes que desea generar"
+			read IMAGENES
+			bash generar.sh $IMAGENES
+			;;
+		2)
+			echo "Has seleccionado descargar"
+			bash descargar.sh
+			;;
+		3)
+			echo "Has seleccionado descomprimir "
+			bash descomprimir.sh
+			;;
+		4)
+			echo "Has seleccionado procesar"
+			bash procesar.sh
+			;;
+		5)
+			echo "Has seleccionado comprimir"
+			bash comprimir.sh
+			;;
+		6)
+			echo "Has seleccionado salir. Haste luego"
+			break
+			;;
+		*)
+			echo "$sal no es una opcion valida"
 
 	esac
 done
