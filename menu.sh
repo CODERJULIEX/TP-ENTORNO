374195ba09c82c843ce271675a7c4625d82bdd62
ignacio <ignacio.cicoria@gmail.com>
2023-07-11 00:45:25 -0300

    le agregue a menu.sh la ejecucion de los archivos

--- menu.sh
+++ menu.sh
@@ -13,14 +13,19 @@ echo "6: Salir"
 	read -p "Seleccione una opcion: " sal
 
 	case $sal in
-		1) echo " Has seleccionado generar imagenes ";;
-		2) echo "Has seleccionado descargar";;
-		3) echo "Has seleccionado descomprimir ";;
-		4) echo "Has seleccionado procesar";;
-		5) echo "Has seleccionado comprimir";;
+		1) echo " Has seleccionado generar imagenes "
+		bash generar.sh;;
+		2) echo "Has seleccionado descargar"
+		bash descargar.sh;;
+		3) echo "Has seleccionado descomprimir "
+		bash descomprimir.sh;;
+		4) echo "Has seleccionado procesar"
+		bash procesar.sh;;
+		5) echo "Has seleccionado comprimir"
+		bash comprimir.sh;;
 		6) echo "Has seleccionado salir. Haste luego"
 		break;;
-		*) echo $sal "no es una opcion"
+		*) echo "$sal no es una opcion valida"
 
 	esac
 done
